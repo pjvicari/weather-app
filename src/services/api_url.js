e03b6fba72a39c007e794940bf04f37f7b47ae5a
pjvicari <33725003+pjvicari@users.noreply.github.com>
2020-07-26 18:25:22 -0600

    Update api_url.js

--- src/services/api_url.js
+++ src/services/api_url.js
@@ -1,4 +1,4 @@
 
-export const api_key = "98d46a7fad1ff0dac34e054de77215f2";
+export const api_key = "";
 export const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
 // export const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
